c80d586f1fd0bcba9cccbd9b9c1785309462ba54
PolBadWolf <gm.gameoff@gmail.com>
2023-05-12 14:57:03 +0500

    init project avr studio

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Send2Rs.cpp
+ *
+ * Created: 12.05.2023 14:49:29
+ * Author : Gumerovmn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
